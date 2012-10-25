24af0153dca596193560e229bd48a7bc078c6d9c
Greg Hendershott <greghendershott@gmail.com>
2012-10-25 10:11:06 -0400

    Use gh.css and use xrefs.

--- make-doc.sh
+++ make-doc.sh
@@ -1,1 +1,1 @@
-scribble --html --dest-name index.html main.rkt
+scribble --html ++style gh.css ++xref-in setup/xref load-collections-xref --redirect-main "http://pre.plt-scheme.org/docs/html/" --dest-name index.html main.rkt
