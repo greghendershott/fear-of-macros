6531d03362feeaadc6229385ae56fbc758c060e3
Greg Hendershott <greghendershott@gmail.com>
2012-11-14 08:57:47 -0500

    Fix cp

--- make-doc.sh
+++ make-doc.sh
@@ -1,3 +1,3 @@
 scribble --htmls ++style gh.css ++xref-in setup/xref load-collections-xref --redirect-main "http://docs.racket-lang.org/" index.rkt
 racket add-to-head.rkt
-cp index/ ./
+cp index/* ./
