7652f3403b36ee4bbc0b67d7ce15e0df18ecd766
Greg Hendershott <greghendershott@gmail.com>
2014-06-19 13:07:32 -0400

    Update 20140619130732

--- push.sh
+++
@@ -1,7 +0,0 @@
-# Simply make gh-pages branch a mirror of master branch.
-git checkout master && \
-git push origin master && \
-git checkout gh-pages && \
-git merge master && \
-git push origin gh-pages && \
-git checkout master
