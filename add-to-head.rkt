5147b894fe670b152a2889978074e905347ad2b3
Greg Hendershott <greghendershott@gmail.com>
2018-06-11 15:25:07 -0400

    rm Google Analytics and Fonts wankery

--- add-to-head.rkt
+++ add-to-head.rkt
@@ -7,38 +7,19 @@
 ;; This reads all HTML files and injects some stuff immediately before the
 ;; </head> closing tag.
 
-(define web-font
-  "<link href='http://fonts.googleapis.com/css?family=Fenix' rel='stylesheet' type='text/css'>")
-
-(define ga-code
-#<<EOF
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-29709446-1']);
-_gaq.push(['_setDomainName', 'greghendershott.com']);
-_gaq.push(['_trackPageview']);
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-</script>
-EOF
-)
-
 (define (meta k v)
   (format "<meta name=\"~a\" content=\"~a\">" k v))
 
 (define metas
   (string-append
-   (meta "keywords" "Racket,macros,Scheme")
+   (meta "keywords" "Racket, macros, Scheme")
    (meta "description" "Practical Racket macros")
    (meta "author" "Greg Hendershott")
    (meta "charset" "utf-8")))
-  
+
 (define </head> "</head>")
 
-(define all (string-append metas web-font ga-code </head>))
+(define all (string-append metas </head>))
 (define subst (regexp-replace* "\n" all "")) ;minify
 
 (define (do-file path)
